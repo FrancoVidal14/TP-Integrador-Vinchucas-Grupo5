20387b93d1cb092f2744b965c47967f61b50c9cb
DamianVillalba <devillalba20@gmail.com>
2025-05-23 16:37:41 -0300

    feat: first commit. Inicio con sistema, muestra, usuario y opiniones

---
+++ TPIntegrador_Vinchucas/src/usuario/Usuario.java
@@ -0,0 +1,34 @@
+package usuario;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import Vinchucas.AplicacionWeb;
+import Vinchucas.Ubicacion;
+import muestra.Muestra;
+
+public abstract class Usuario {
+	
+	private List<Opinion> opiniones;
+	private NivelConocimiento nivelConocimiento;
+	
+	public Usuario() {
+		this.opiniones = new ArrayList<>();
+	}
+	
+	//suponemos que siempre que se envie una muestra, es en el momento exacto. Como un boton "enviar" por lo que la fecha es la que capta en ese momento
+	public void enviarMuestra(Ubicacion ubicacionOrigen, Opinion opinionUsuario, AplicacionWeb sistema) {
+		Muestra muestra = new Muestra(LocalDateTime.now(), ubicacionOrigen, this, opinionUsuario);
+		sistema.recibirMuestra(muestra);
+		opiniones.add(opinionUsuario);
+	}
+	
+	public void opinar(Muestra muestra, Opinion opinionUsuario) {
+		muestra.procesarOpinion(opinionUsuario);
+	}
+	
+	public NivelConocimiento getNivelConocimiento() {
+		return this.nivelConocimiento;
+	}
+}
