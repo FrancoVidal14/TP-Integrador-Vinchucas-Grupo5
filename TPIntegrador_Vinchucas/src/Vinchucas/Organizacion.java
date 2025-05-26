46358466d7f1445b372fc4ebbdfccc5f8db517f7
Franco <franconicolasvidal@gmail.com>
2025-05-26 17:24:15 -0300

    Organizacion corriendo

--- TPIntegrador_Vinchucas/src/Vinchucas/Organizacion.java
+++ TPIntegrador_Vinchucas/src/Vinchucas/Organizacion.java
@@ -1,5 +1,39 @@
 package Vinchucas;
 
 public class Organizacion {
+	private Ubicacion ubicacion;
+	private TipoDeOrganizacion tipo;
+	private int cantEmpleados;
+	
+	public Organizacion(Ubicacion ubicacion, TipoDeOrganizacion tipo, int cantEmpleados) {
+		this.setUbicacion(ubicacion);
+		this.setTipo(tipo);
+		this.setCantEmpleados(cantEmpleados);
+	}
+
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(Ubicacion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public int getCantEmpleados() {
+		return cantEmpleados;
+	}
+
+	public void setCantEmpleados(int cantEmpleados) {
+		this.cantEmpleados = cantEmpleados;
+	}
+
+	public TipoDeOrganizacion getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoDeOrganizacion tipo) {
+		this.tipo = tipo;
+	}
+
 
 }
