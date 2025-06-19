fa4f01070ebc98ebbfe16e3e5ff9347bfa752020
DamianVillalba <devillalba20@gmail.com>
2025-06-19 13:18:14 -0300

    fix: hotfix de cumpleCondiciones en Recategorizador

--- TPIntegrador_Vinchucas/src/Vinchucas/Recategorizador.java
+++ TPIntegrador_Vinchucas/src/Vinchucas/Recategorizador.java
@@ -20,10 +20,10 @@ public class Recategorizador {
 	}
 
     public boolean cumpleCondiciones(Usuario usuario, int cantDiasConsiderados, int cantEnviosEsperados, int cantRevisionesEsperadas) {
-        return this.cantidadEnvios(usuario, cantDiasConsiderados) > cantEnviosEsperados && this.cantidadRevisionesExitosas(usuario,cantDiasConsiderados) > cantRevisionesEsperadas;
+        return this.cantidadEnviosDeEn(usuario, cantDiasConsiderados) >= cantEnviosEsperados && this.cantidadRevisionesExitosasDeEn(usuario,cantDiasConsiderados) >= cantRevisionesEsperadas;
     }
     
-    private long cantidadEnvios(Usuario usuario, int cantDiasConsiderados){
+    private long cantidadEnviosDeEn(Usuario usuario, int cantDiasConsiderados){
     	return datos.getMuestrasEnviadasPor(usuario).stream()
         .filter(m -> m.generadaEnUltimos(cantDiasConsiderados))
         .count();
@@ -37,7 +37,7 @@ public class Recategorizador {
     }
     */
     
-    private int cantidadRevisionesExitosas(Usuario usuario, int dias) {
+    private int cantidadRevisionesExitosasDeEn(Usuario usuario, int dias) {
         return datos.getMuestrasEnviadasPor(usuario).stream()
             .filter(m -> m.usuarioHizoRevisionExitosa(usuario, dias))
             .toList().size();
