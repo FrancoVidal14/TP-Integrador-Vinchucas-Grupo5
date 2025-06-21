d228fc4b8d5c679bdda4e467514238ede74f2c9b
Franco <franconicolasvidal@gmail.com>
2025-06-21 13:54:47 -0300

    Algunos formateos de codigo en relacion al UML

--- TPIntegrador_Vinchucas/src/Vinchucas/Organizacion.java
+++ TPIntegrador_Vinchucas/src/Vinchucas/Organizacion.java
@@ -2,14 +2,16 @@ package Vinchucas;
 
 import muestra.Muestra;
 
-public class Organizacion implements NotificadosPorMuestras{
+public class Organizacion implements NotificadosPorMuestras {
 	private Ubicacion ubicacion;
 	private TipoDeOrganizacion tipo;
 	private int cantEmpleados;
 	private FuncionalidadExterna funcionalidadRegistro;
 	private FuncionalidadExterna funcionalidadValidacion;
-	
-	public Organizacion(Ubicacion ubicacion, TipoDeOrganizacion tipo, int cantEmpleados, FuncionalidadExterna fun1, FuncionalidadExterna fun2) {
+
+	// Constructor
+	public Organizacion(Ubicacion ubicacion, TipoDeOrganizacion tipo, int cantEmpleados,
+	                    FuncionalidadExterna fun1, FuncionalidadExterna fun2) {
 		this.setUbicacion(ubicacion);
 		this.setTipo(tipo);
 		this.setCantEmpleados(cantEmpleados);
@@ -17,36 +19,55 @@ public class Organizacion implements NotificadosPorMuestras{
 		this.setFuncionalidadValidacion(fun2);
 	}
 
+	// Getters
 	public Ubicacion getUbicacion() {
 		return ubicacion;
 	}
 
-    public void setUbicacion(Ubicacion ubicacion) {
-        if (ubicacion == null) {
-            throw new IllegalArgumentException("La ubicación no puede ser nula");
-        }
-        this.ubicacion = ubicacion;
-    }
+	public TipoDeOrganizacion getTipo() {
+		return tipo;
+	}
 
 	public int getCantEmpleados() {
 		return cantEmpleados;
 	}
 
-	public void setCantEmpleados(int cantEmpleados) {
-	    if (cantEmpleados < 0) {
-	        throw new IllegalArgumentException("La cantidad de empleados no puede ser negativa");
-	    }
-	    this.cantEmpleados = cantEmpleados;
+	public FuncionalidadExterna getFuncionalidadRegistro() {
+		return funcionalidadRegistro;
 	}
-	
-	public TipoDeOrganizacion getTipo() {
-		return tipo;
+
+	public FuncionalidadExterna getFuncionalidadValidacion() {
+		return funcionalidadValidacion;
+	}
+
+	// Setters
+	public void setUbicacion(Ubicacion ubicacion) {
+		if (ubicacion == null) {
+			throw new IllegalArgumentException("La ubicación no puede ser nula");
+		}
+		this.ubicacion = ubicacion;
 	}
 
 	public void setTipo(TipoDeOrganizacion tipo) {
 		this.tipo = tipo;
 	}
 
+	public void setCantEmpleados(int cantEmpleados) {
+		if (cantEmpleados < 0) {
+			throw new IllegalArgumentException("La cantidad de empleados no puede ser negativa");
+		}
+		this.cantEmpleados = cantEmpleados;
+	}
+
+	public void setFuncionalidadRegistro(FuncionalidadExterna funcionalidadRegistro) {
+		this.funcionalidadRegistro = funcionalidadRegistro;
+	}
+
+	public void setFuncionalidadValidacion(FuncionalidadExterna funcionalidadValidacion) {
+		this.funcionalidadValidacion = funcionalidadValidacion;
+	}
+
+	// Métodos de notificación
 	@Override
 	public void recibirNotificacionDeRegistroDe(Muestra m, ZonaDeCobertura zona) {
 		getFuncionalidadRegistro().nuevoEvento(this, zona, m);
@@ -55,21 +76,5 @@ public class Organizacion implements NotificadosPorMuestras{
 	@Override
 	public void recibirNotificacionDeValidacionDe(Muestra m, ZonaDeCobertura z) {
 		getFuncionalidadValidacion().nuevoEvento(this, z, m);
-	}
-
-	public FuncionalidadExterna getFuncionalidadRegistro() {
-		return funcionalidadRegistro;
-	}
-
-	public void setFuncionalidadRegistro(FuncionalidadExterna funcionalidadRegistro) {
-		this.funcionalidadRegistro = funcionalidadRegistro;
-	}
-
-	public FuncionalidadExterna getFuncionalidadValidacion() {
-		return funcionalidadValidacion;
-	}
-
-	public void setFuncionalidadValidacion(FuncionalidadExterna funcionalidadValidacion) {
-		this.funcionalidadValidacion = funcionalidadValidacion;
 	}
 }
